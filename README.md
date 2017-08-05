4d774336abca85af79d620ba4b7f0542f14ab437
richardse08 <eric.richards.finance@gmail.com>
2017-08-05 12:16:07 -0400

    Added some comments and reviewed where I could improve this project, see readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # wikipedia-viewer
+
 The Wikipedia Viewer is a simple interface that uses API calls, AJAX and JSON to display "top-ten" results of input
+
+
+# Things I could have done differently
+
+I could definitely have done a few things differently for this project, specifically instead of using <br> I could have leveraged CSS to create divs for the results that would only show when the enter icon is clicked
+
+Tie in some JavaScript to allow the enter key to fire the request
+
+Consider using media queries to create a better mobile experience
+
+Re-style the buttons and add wikipedia logo
--- script.js
+++ script.js
@@ -1,34 +1,47 @@
         
 $(document).ready(function () {
     
-    $("#text_value").click(function() { 
-        // Reset search results
-        $(".target1").html('');
-        // Build out ajax request
+    
+    $("#search-button").click(function() { 
+        
+        // First, reset search results so we're not stacking them over each other
+        $(".target").html('');
+        
+        // Create string for get request using user inputted value
         var beginStr = "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search="; 
         var queryStr = document.querySelector('#textinput').value;
         var endStr = beginStr + queryStr + "&generator=allpages&gaplimit=10&prop=info&format=json&formatversion=2"; 
        
         $.get(endStr, function(myObj) {
+            
             // Add border to the search results
-            $(".target1").addClass("add-border");
+            $(".target").addClass("add-border");
+            
             // Loop from 0 to 9 to show a total of 10 results
             for (i = 0; i < 10; i++) {
-
-                $(".target1").append(myObj[1][i]);
-                $(".target1").append("<br>");
-                $(".target1").append(myObj[2][i]);
-                $(".target1").append("<br>");
-                $(".target1").append("<a href='" + myObj[3][i] + "'>Click for Full Article</a>");
-                $(".target1").append("<br>");
-                $(".target1").append("<br>");
+                
+                // Show title of article
+                $(".target").append(myObj[1][i]);
+                $(".target").append("<br>");
+                
+                // Show description of article
+                $(".target").append(myObj[2][i]);
+                $(".target").append("<br>");
+                
+                // Show link to wikipedia page
+                $(".target").append("<a href='" + myObj[3][i] + "'>Click for Full Article</a>");
+                $(".target").append("<br>");
+                $(".target").append("<br>");
                 
             };
+            
 
         }); //close .get
       
+        
     }); //close button clicker
   
+    
 }); //doc.ready end
     
 
